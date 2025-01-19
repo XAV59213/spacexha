4199c089049c55d7e701f7ec960e9e2f2724636a
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-01-19 10:24:00 +0100

    Delete custom_components/spacex directory

--- custom_components/spacex/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the SpaceX integration."""
-
-DOMAIN = "spacexX"
-COORDINATOR = "coordinator"
-SPACEX_API = "spacex_api"
-ATTR_IDENTIFIERS = "identifiers"
-ATTR_MANUFACTURER = "manufacturer"
-ATTR_MODEL = "model"
