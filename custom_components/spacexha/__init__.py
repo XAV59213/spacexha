c6b1207f8bf5b886596ded3c9b6b443b5c0ad940
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-01-21 16:06:12 +0100

    Update __init__.py

--- custom_components/spacexha/__init__.py
+++ custom_components/spacexha/__init__.py
@@ -3,7 +3,7 @@ import asyncio
 from datetime import timedelta
 import logging
 
-from spacexpypi import Spacex
+from spacexpypi import SpaceX
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
